257f76da2cfeb2853a9f9e8e36d4748553b34a61
y-hrubskyi <y.hrubskyi@gmail.com>
2023-12-01 22:58:53 +0200

    some updates

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,25 +1,34 @@
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { selectFilteredContacts } from 'redux/contacts/selectors';
 import { fetchContacts } from 'redux/contacts/operations';
+import { selectFilter } from 'redux/filter/selectors';
 
 import { ContactsList } from './ContactList.styled';
-import { useEffect } from 'react';
 import { Contact } from 'components/Contact/Contact';
 
 export const ContactList = () => {
   const dispatch = useDispatch();
   const contacts = useSelector(selectFilteredContacts);
+  const filter = useSelector(selectFilter);
 
   useEffect(() => {
     dispatch(fetchContacts());
   }, [dispatch]);
 
-  return (
+  let filterInfo = '';
+  const results = contacts.length;
+  if (!results && !filter) filterInfo = <p>Your contact list is empty</p>;
+  if (!results && filter) filterInfo = <p>Not Finded</p>;
+
+  return contacts.length ? (
     <ContactsList>
       {contacts.map(contact => (
         <Contact key={contact.id} contact={contact} />
       ))}
     </ContactsList>
+  ) : (
+    filterInfo
   );
 };
--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -2,12 +2,16 @@ import { ContactForm } from 'components/ContactForm/ContactForm';
 import { ContactList } from 'components/ContactList/ContactList';
 import { Filter } from 'components/Filter/Filter';
 import { Title } from 'components/App.styled';
+import { useSelector } from 'react-redux';
+import { selectContacts } from 'redux/contacts/selectors';
 
 const Contacts = () => {
+  const contacts = useSelector(selectContacts);
+
   return (
     <>
       <Title>Contacts List</Title>
-      <Filter />
+      {contacts.length > 0 && <Filter />}
       <ContactList />
       <ContactForm />
     </>
