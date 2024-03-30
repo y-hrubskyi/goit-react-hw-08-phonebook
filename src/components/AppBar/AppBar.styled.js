bc5fb098c6a47c501361c93f3647bf47acfa1286
y-hrubskyi <y.hrubskyi@gmail.com>
2024-03-30 19:42:06 +0200

    refactor: upd AppBar styles

--- src/components/AppBar/AppBar.styled.js
+++ src/components/AppBar/AppBar.styled.js
@@ -4,6 +4,7 @@ export const Header = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin-bottom: 16px;
-  border-bottom: 1px solid #2a363b;
+  margin-bottom: ${p => p.theme.spacing(4)};
+  padding-top: ${p => p.theme.spacing(5)};
+  margin-bottom: ${p => p.theme.spacing(5)};
 `;
