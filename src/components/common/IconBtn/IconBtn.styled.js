38b79b27ef6253d9f39b6a0669936b6514199454
y-hrubskyi <y.hrubskyi@gmail.com>
2024-03-31 17:42:07 +0300

    refactor: create reusable IconBtn component

---
+++ src/components/common/IconBtn/IconBtn.styled.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 36px;
+  height: 36px;
+
+  color: ${p => p.theme.colors.accent};
+
+  background-color: ${p => p.theme.colors.white};
+  border-radius: ${p => p.theme.radii.md};
+  border: 1px solid ${p => p.theme.colors.accent};
+
+  transition: ${p => p.theme.transition('color')},
+    ${p => p.theme.transition('background-color')};
+
+  &:hover,
+  &:focus {
+    color: ${p => p.theme.colors.white};
+    background-color: ${p => p.theme.colors.accent};
+  }
+`;
