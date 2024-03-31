62dcfc1ab3bc025225ea52fed13d846da5d87a24
y-hrubskyi <y.hrubskyi@gmail.com>
2024-03-31 21:39:27 +0300

    feat: add bg for pages

--- src/components/Layout/Layout.styled.js
+++ src/components/Layout/Layout.styled.js
@@ -1,16 +1,24 @@
 import styled from 'styled-components';
 
+import Background from 'assets/img/bg.jpg';
+
 export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
   max-width: 960px;
   margin: 0 auto;
+  min-height: 100vh;
   padding-left: ${p => p.theme.spacing(5)};
   padding-right: ${p => p.theme.spacing(5)};
+
+  background-image: url(${Background});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
 `;
 
 export const Main = styled.main`
   display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding-top: ${p => p.theme.spacing(5)};
-  padding-bottom: ${p => p.theme.spacing(10)};
+  justify-content: center;
+  flex-grow: 1;
 `;
