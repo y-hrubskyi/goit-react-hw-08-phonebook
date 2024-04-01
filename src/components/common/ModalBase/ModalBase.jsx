b57908a81074c97539810cb71e150d01b2dfbf79
y-hrubskyi <y.hrubskyi@gmail.com>
2024-04-01 13:04:14 +0300

    refactor: upd ModalBase styles

--- src/components/common/ModalBase/ModalBase.jsx
+++ src/components/common/ModalBase/ModalBase.jsx
@@ -1,15 +1,6 @@
 import Modal from 'react-modal';
 
-const modalStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-  },
-};
+import { modalStyles } from './ModalBase.styled';
 
 Modal.setAppElement('#root');
 
